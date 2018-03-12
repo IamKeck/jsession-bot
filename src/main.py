72e0e9edca797500d4f25890ea979fa86857cf64
Kent Kawanaka <k-kawanaka@plott.co.jp>
2018-03-12 13:00:06 +0900

    曲投稿機能及び曲リストを追加

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from os import environ
 import tweepy
 import datetime
+import json
+from functools import reduce
+import random
 
 # Twitterオブジェクトの生成
 auth = tweepy.OAuthHandler(environ["CK"], environ["CS"])
@@ -10,11 +15,17 @@ auth.set_access_token(environ["AT"], environ["AS"])
 api = tweepy.API(auth)
 
 class Listener(tweepy.StreamListener):
+    def set_song_list(self, song_list):
+        self._song_list = song_list
+
+    def select_song(self):
+        return random.choice(self._song_list)
+
     def on_status(self, status):
         status.created_at += datetime.timedelta(hours=9)
 
         if str(status.in_reply_to_screen_name)== "jsession_bot":
-            tweet = "@" + str(status.user.screen_name) + " " + "Hello！\n" + str(datetime.datetime.today())
+            tweet = "@{} 次は{}なんていかがでしょうか?".format(status.user.screen_name, self.select_song().upper())
             api.update_status(status=tweet, in_reply_to_status_id=status.id)
         return True
 
@@ -26,7 +37,13 @@ class Listener(tweepy.StreamListener):
         print('Timeout...')
         return True
 
+with open("./songs.json", "r") as songs:
+    song_data = json.load(songs)
+
+song_list = reduce(lambda acc, d: acc + [d["name"]] * d["rate"], song_data, [])
 
 listener = Listener()
+listener.set_song_list(song_list)
 stream = tweepy.Stream(auth, listener)
 stream.filter(track=["@jsession_bot"])
+
